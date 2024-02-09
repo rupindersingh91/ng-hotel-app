b7704887cc38a53405bdf7979093ef340231485f
Rupinder Singh <rupindersingh0853@gmail.com>
2024-02-09 17:42:15 +0530

    Used local storage in reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,8 +8,14 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
   // CRUD
   getReservations(): Reservation[] {
+    this.reservations = JSON.parse(localStorage.getItem("reservations") || "[]");
     return this.reservations;
   }
 
@@ -19,17 +25,19 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
-    console.log(this.reservations);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(updateReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === updateReservation.id);
     this.reservations[index] = updateReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
 }
