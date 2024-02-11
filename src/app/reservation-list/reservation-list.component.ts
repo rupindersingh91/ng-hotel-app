01b2475c550cd2c9b23bf8d0d17c2e2a405e103f
Rupinder Singh <rupindersingh0853@gmail.com>
2024-02-11 17:34:06 +0530

    Delete reservation added

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -17,4 +17,8 @@ export class ReservationListComponent implements OnInit {
     this.reservations = this.reservationService.getReservations();
   }
 
+  deleteReservation(id: string): void {
+    this.reservationService.deleteReservation(id);
+  }
+
 }
