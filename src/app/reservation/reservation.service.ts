beec19fb12515315877781c3eee5f8dae8a0dfdc
Rupinder Singh <techformation.rupinder@gmail.com>
2024-02-29 10:29:16 +0530

    Removed code to save data in local storage

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,14 +8,9 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
-  constructor() {
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-  }
 
   // CRUD
   getReservations(): Reservation[] {
-    this.reservations = JSON.parse(localStorage.getItem("reservations") || "[]");
     return this.reservations;
   }
 
@@ -26,19 +21,16 @@ export class ReservationService {
   addReservation(reservation: Reservation): void {
     reservation.id = Date.now().toString();
     this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updateReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations[index] = updateReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
 }
